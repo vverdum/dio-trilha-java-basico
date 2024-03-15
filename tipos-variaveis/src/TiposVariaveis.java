245e52490b65d7c15d43c71a69ed625f954e3c59
Vverdum <vverdum@gmail.com>
2024-03-15 11:51:35 -0300

    Sobre concatenacao de strings

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioFuncionario = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroNormal);
+
+        
     }
 }

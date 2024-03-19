b5aa0f9f24f57fb5c586d5254f73b3a36b06f602
Vverdum <vverdum@gmail.com>
2024-03-18 21:04:22 -0300

    adicion java doc

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,11 +1,16 @@
 public class Usuario {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         
+        @SuppressWarnings("unused")
         SmartTv smartTv = new SmartTv();
 
         System.out.println("TV smart ligada? "+ SmartTv.ligada);
-        System.out.println("Qual é o canal atual? "+ smartTv.canal);
-        System.out.println("Em que volume está atualmente? "+ smartTv.volume);
+        System.out.println("Qual é o canal atual? "+ SmartTv.canal);
+        System.out.println("Em que volume está atualmente? "+ SmartTv.volume);
 
         SmartTv.ligar();
         System.out.println("TV smart ligada agora? "+ SmartTv.ligada); 
@@ -20,7 +25,7 @@ public class Usuario {
         SmartTv.diminuirVolume();
         SmartTv.diminuirVolume();
         SmartTv.aumentarVolume();
-        System.out.println("Em que volume está atualmente? "+ smartTv.volume);
+        System.out.println("Em que volume está atualmente? "+ SmartTv.volume);
 
 
 

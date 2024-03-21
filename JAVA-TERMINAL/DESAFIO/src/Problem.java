ada2c8cc1e3626ee15756858fb537ba71e9062be
Vverdum <vverdum@gmail.com>
2024-03-21 15:30:16 -0300

    Estudo de terminais e argumentos

---
+++ JAVA-TERMINAL/DESAFIO/src/Problem.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		 //Complete os espaços em branco com uma possível solução para o desafio 
+		PROD =  A*B;  
+
+		System.out.println("PROD = " +  PROD   );
+	}
+}
